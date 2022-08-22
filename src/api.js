6db603b13c30f9207c383eebe5319ff181fcfa8f
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-22 14:12:44 +0100

    Modified fetchTopics to axios function

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
+import axios from 'axios'
+
 export const fetchArticles = (topic) => {
-  console.log(topic, "topic in api");
   if (topic === "All") {
     return fetch("https://news-api-08-22.herokuapp.com/api/articles").then(
       (res) => {
@@ -7,10 +8,29 @@ export const fetchArticles = (topic) => {
       }
     );
   } else {
-    return fetch(`https://news-api-08-22.herokuapp.com/api/articles?topic=${topic}`).then(
-        (res) => {
-          return res.json();
-        }
-      )
+    return fetch(
+      `https://news-api-08-22.herokuapp.com/api/articles?topic=${topic}`
+    ).then((res) => {
+      return res.json();
+    });
   }
+};
+
+// export const fetchTopics = () => {
+//   return fetch("https://news-api-08-22.herokuapp.com/api/topics").then(
+//     (res) => {
+//       return res.json();
+//     }
+//   );
+// };
+
+export const fetchTopics = () => {
+  return axios
+    .get("https://news-api-08-22.herokuapp.com/api/topics")
+    .then((res) => {
+        return res.data
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
