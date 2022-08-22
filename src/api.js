8954d3e2c5e70ee5d415821eee90c8738ed7426a
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-22 12:42:48 +0100

    Added conditionals to api request

--- src/api.js
+++ src/api.js
@@ -1,7 +1,16 @@
-export const fetchArticles = () => {
+export const fetchArticles = (topic) => {
+  console.log(topic, "topic in api");
+  if (topic === "All") {
     return fetch("https://news-api-08-22.herokuapp.com/api/articles").then(
       (res) => {
         return res.json();
       }
     );
-  };
+  } else {
+    return fetch(`https://news-api-08-22.herokuapp.com/api/articles?topic=${topic}`).then(
+        (res) => {
+          return res.json();
+        }
+      )
+  }
+};
