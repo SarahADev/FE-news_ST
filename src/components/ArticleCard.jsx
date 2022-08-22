4b9684544244119bdbc58235cea27ff86baa3852
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-22 14:14:12 +0100

    Added key attribute to articlecard component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,7 @@
-const ArticleCard = ({title, author, topic, votes, comment_count, date}) => {
+const ArticleCard = ({article_id, title, author, topic, votes, comment_count, date}) => {
     const formatDate = date.slice(0, -14)
     return (
-        <section className="article-card">
+        <section className="article-card" key={article_id}>
             <h2 className="article-title">{title}</h2>
             <span className="article-author-topic">BY: {author} , ON: {topic}</span> <br/>
             <span className="article-votes-comments">Votes: {votes}, Comments: {comment_count}</span> <br/>
