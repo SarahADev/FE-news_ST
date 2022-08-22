ac56d45d3882d662dfe187c6f3c474d54114735e
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-22 11:58:17 +0100

    Created placeholder page components

---
+++ src/components/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <section className="homepage">
+            <h2>Welcome to NC News</h2>
+        </section>
+    )
+}
+
+export default Home;
---
+++ src/components/UserList.jsx
@@ -0,0 +1,9 @@
+const UserList = () => {
+    return (
+        <section className="user-list">
+            <h2>Users page</h2>
+        </section>
+    )
+}
+
+export default UserList;
