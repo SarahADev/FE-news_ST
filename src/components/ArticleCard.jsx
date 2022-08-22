0aea7f72456edb581f893030bde8b19a02e9879c
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-22 16:40:17 +0100

    Added VIEW button to each article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,13 +1,41 @@
-const ArticleCard = ({article_id, title, author, topic, votes, comment_count, date}) => {
-    const formatDate = date.slice(0, -14)
-    return (
-        <section className="article-card">
-            <h2 className="article-title">{title}</h2>
-            <span className="article-author-topic">BY: {author} , ON: {topic}</span> <br/>
-            <span className="article-votes-comments">Votes: {votes}, Comments: {comment_count}</span> <br/>
-            <span className="article-date">{formatDate}</span>
-        </section>
-    )
-}
+import { useNavigate } from "react-router-dom";
+
+const ArticleCard = ({
+  article_id,
+  title,
+  author,
+  topic,
+  votes,
+  comment_count,
+  date,
+}) => {
+  const formatDate = date.slice(0, -14);
+  let navigate = useNavigate();
+  const handleView = (article_id) => {
+    console.log(article_id, "article_id in handle");
+    navigate(`/article/${article_id}`);
+  };
+  return (
+    <section className="article-card">
+      <h2 className="article-title">{title}</h2>
+      <span className="article-author-topic">
+        BY: {author} , ON: {topic}
+      </span>{" "}
+      <br />
+      <span className="article-votes-comments">
+        Votes: {votes}, Comments: {comment_count}
+      </span>{" "}
+      <br />
+      <span className="article-date">{formatDate}</span> <br />
+      <button
+        onClick={() => {
+          handleView(article_id);
+        }}
+      >
+        VIEW
+      </button>
+    </section>
+  );
+};
 
 export default ArticleCard;
