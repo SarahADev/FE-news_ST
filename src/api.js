0ed44d907b00d6e34e98a0a6105194351a91de3b
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-23 08:59:36 +0100

    Added PATCH articlevote function

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const fetchArticles = (topic) => {
   if (topic === "All") {
@@ -28,7 +28,18 @@ export const fetchTopics = () => {
   return axios
     .get("https://news-api-08-22.herokuapp.com/api/topics")
     .then((res) => {
-        return res.data
+      return res.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const fetchSingleArticle = (article_id) => {
+  return axios
+    .get(`https://news-api-08-22.herokuapp.com/api/articles/${article_id}`)
+    .then((res) => {
+      return res.data;
     })
     .catch((err) => {
       console.log(err);
@@ -36,13 +47,15 @@ export const fetchTopics = () => {
 };
 
 
-export const fetchSingleArticle = (article_id) => {
-    return axios
-      .get(`https://news-api-08-22.herokuapp.com/api/articles/${article_id}`)
-      .then((res) => {
-          return res.data
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
+export const updateArticleVotes = (article_id) => {
+  return axios
+    .patch(`https://news-api-08-22.herokuapp.com/api/articles/${article_id}`, {
+      inc_votes : 1
+    })
+    .then((res) => {
+      console.log(res, 'res in patch')
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
