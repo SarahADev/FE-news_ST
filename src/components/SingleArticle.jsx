6421d1d0ec325ecfaeab42e61da95840bfe92ad5
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-24 11:56:56 +0100

    Renders DeleteComment

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,35 +1,38 @@
 import { useParams } from "react-router-dom";
-import { useState, useEffect} from "react";
-import { fetchArticleComments, fetchSingleArticle} from "../api";
+import { useState, useEffect, useContext } from "react";
+import { fetchArticleComments, fetchSingleArticle } from "../api";
 import ArticleVote from "./ArticleVote";
 import ArticleComments from "./ArticleComments";
 import PostComment from "./PostComment";
-
-
+import { UserContext } from "../contexts/User";
+import DeleteComment from "./DeleteComment";
 
 const SingleArticle = () => {
+  const { user, setUser } = useContext(UserContext);
   const { article_id } = useParams();
   const [singleArticle, setSingleArticle] = useState();
   const [articleCommentList, setArticleCommentList] = useState([]);
-  const [err, setErr] = useState(null)
+  const [err, setErr] = useState(null);
 
   useEffect(() => {
-    setErr(null)
-    fetchSingleArticle(article_id).then(({ article }) => {
-      setSingleArticle(article);
-    })
-    .catch(() =>{
-      setErr('Something went wrong...')
-    })
-    fetchArticleComments(article_id).then(({ comments }) => {
-      setArticleCommentList(comments);
-    })
-    .catch(() => {
-      setErr('Something went wrong...')
-    })
+    setErr(null);
+    fetchSingleArticle(article_id)
+      .then(({ article }) => {
+        setSingleArticle(article);
+      })
+      .catch(() => {
+        setErr("Something went wrong...");
+      });
+    fetchArticleComments(article_id)
+      .then(({ comments }) => {
+        setArticleCommentList(comments);
+      })
+      .catch(() => {
+        setErr("Something went wrong...");
+      });
   }, [article_id]);
 
-  if(err) return <span> {err} </span>
+  if (err) return <span> {err} </span>;
 
   if (!singleArticle) {
     return <section className="single-article">Loading..</section>;
@@ -46,17 +49,23 @@ const SingleArticle = () => {
       <ArticleVote article_id={article_id} votes={singleArticle.votes} />
       <section className="comment-list">
         <h3>Comments:</h3>
-          <PostComment article_id={article_id} setArticleCommentList={setArticleCommentList}/>
+        <PostComment
+          article_id={article_id}
+          setArticleCommentList={setArticleCommentList}
+        />
         {articleCommentList.map(
           ({ comment_id, votes, created_at, author, body }) => {
             return (
-              <ArticleComments
-                key={comment_id}
-                votes={votes}
-                created_at={created_at}
-                author={author}
-                body={body}
-              />
+              <section key={comment_id}>
+                <ArticleComments
+                  votes={votes}
+                  created_at={created_at}
+                  author={author}
+                  body={body}
+                />
+                {author === user ? <DeleteComment setArticleCommentList={setArticleCommentList} comment_id={comment_id}/> : null}
+                
+              </section>
             );
           }
         )}
