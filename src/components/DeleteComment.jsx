fee7d4694784bdc801b9946d60ae812c2c5fa7a0
Sarah Alexa <sarahalexa@Sarahs-MBP.lan>
2022-08-24 11:56:31 +0100

    Added DeleteComment component to render Delete button for users comments and handle deletion

---
+++ src/components/DeleteComment.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { deleteArticleComment } from "../api";
+const DeleteComment = ({ setArticleCommentList, comment_id }) => {
+  const [err, setErr] = useState(null);
+
+  const handleDeleteComment = () => {
+    deleteArticleComment(comment_id)
+      .then(() => {
+        setArticleCommentList((currArticleCommentList) => {
+          let modifiedCommentList = [...currArticleCommentList];
+          return modifiedCommentList.filter(
+            (comment) => comment.comment_id !== comment_id
+          );
+        });
+      })
+      .catch((err) => {
+        setErr(err.message);
+      });
+  };
+
+  if (err) return <span>{err} </span>;
+  return (
+    <button className="delete-comment" onClick={handleDeleteComment}>
+      {" "}
+      DELETE{" "}
+    </button>
+  );
+};
+
+export default DeleteComment;
